8598216564b5db3fa683c7b7345230c0a312cd7c
RYiOSDev <1859295+ryiosdev@users.noreply.github.com>
2025-03-15 14:40:17 -0500

    cachableImage improvements towards refactoring it to have its own VM

--- ryweather/ViewModels/ViewModelConfiguration.swift
+++ ryweather/ViewModels/ViewModelConfiguration.swift
@@ -26,13 +26,17 @@ struct DefaultViewModelConfig: ViewModelConfiguration {
     var weatherDataProvider: WeatherDataProvider
     
     init(inMemoryOnly: Bool = false, weatherAPIKey: String) {
+
         //Use this code to blow away the macOS app's previous SwiftData store.
-        let urlApp = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).last
-        let url = urlApp!.appendingPathComponent("default.store")
-        if FileManager.default.fileExists(atPath: url.path) {
-            print("macOS: delete the swiftData .store* files here if crash on launch: \(url.absoluteString)")
-        }
-        let schema = Schema([LocationModel.self])
+//        #if os(macOS)
+//        let urlApp = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).last
+//        let url = urlApp!.appendingPathComponent("default.store")
+//        if FileManager.default.fileExists(atPath: url.path) {
+//            print("macOS: delete the swiftData .store* files here if crash on launch: \(url.absoluteString)")
+//        }
+//        #endif
+        
+        let schema = Schema([LocationModel.self, CachedImage.self])
         let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: inMemoryOnly)
         do {
             modelContainer = try ModelContainer(for: schema, configurations: [config])
--- ryweather/Views/CacheableImage.swift
+++ ryweather/Views/CacheableImage.swift
@@ -6,32 +6,103 @@
 //
 
 import SwiftUI
+import SwiftData
 
-fileprivate var cache: [URL: Image] = [:]
+fileprivate var memCache: [URL: Image] = [:]
+
+
+@Model
+class CachedImage {
+    @Attribute(.unique) var url: URL
+    @Attribute(.externalStorage) var imageData: Data
+    
+    init(url: URL, imageData: Data) {
+        self.url = url
+        self.imageData = imageData
+    }
+}
 
 struct CacheableImage: View {
+    @Environment(\.modelContext) var modelContext
+
     let url: URL
+    
+    @Query private var swCachedImages: [CachedImage]
+
+    init(url: URL) {
+        self.url = url
         
+        let predicate = #Predicate<CachedImage> { $0.url == url }
+        _swCachedImages = Query(filter: predicate)
+    }
+    
     var body: some View {
-        if let cachedImage = cache[url] {
+        if let swCachedImage = swCachedImages.first,
+           let image = imageFrom(data: swCachedImage.imageData) {
+            image
+                .onAppear {
+                    logger.debug("Reusing existing SwiftData image for : \(url.absoluteString)")
+                }
+        } else if let cachedImage = memCache[url] {
             cachedImage
                 .onAppear {
-                    logger.debug("Reusing existing image for : \(url.absoluteString)")
+                    logger.debug("Reusing existing memCache image for : \(url.absoluteString)")
                 }
         } else {
             AsyncImage(url: url) { phase in
                 if let image = phase.image {
                     image
                         .onAppear {
-                            cache[url] = image
-                            logger.debug("Cached new image for : \(url.absoluteString)")
+                            cache(image: image)
                         }
                 } else if phase.error != nil {
                     EmptyView()
+                    Image(systemName: "")
                 } else {
                     ProgressView()
                 }
             }
         }
+    }
+    
+    private func imageFrom(data: Data) -> Image? {
+#if os(macOS)
+        if let nsImage = NSImage(data: data) {
+            return Image(nsImage: nsImage)
+        }
+#else // TODO: assuming this works for all other platforms.
+        if let uiImage = UIImage(data: data) {
+            return Image(uiImage: uiImage)
+        }
+#endif
+        return nil
+    }
+    
+    private func cache(image: Image) {
+        //memCache[url] = image
+        let imageRenderer = ImageRenderer(content: image)
+        var data: Data?
+#if os(macOS)
+        if let nsImage = imageRenderer.nsImage {
+            data = nsImage.tiffRepresentation // NOTE: saved as .tiff
+        }
+#else
+        if let uiImage = imageRenderer.uiImage {
+            data = uiImage.pngData() // NOTE: saved as .png
+        }
+#endif
+        guard let data else {
+            logger.error("unable to create data for image")
+            return
+        }
+        
+        let cachedImage = CachedImage(url: self.url, imageData: data)
+        modelContext.insert(cachedImage)
+        do {
+            try modelContext.save()
+        } catch {
+            logger.error("Error saving cached image: \(error)")
+        }
+        logger.debug("Cached new image to SwiftData for : \(url.absoluteString)")
     }
 }
